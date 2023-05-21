9ddb968753c4614ae5ed32ab2017cabd38ab4927
Niels Lange <info@nielslange.de>
2023-05-21 19:43:48 +0700

    Add hero image

--- front-page.php
+++ front-page.php
@@ -22,6 +22,12 @@ get_header();
 if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
+		?>
+		<div class="section full">
+			<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/logo.png' ); ?>" alt="Logo" class="logo">
+			<div class="overlay"></div>
+		</div>
+		<?php
 		if ( have_rows( 'sections' ) ) {
 			while ( have_rows( 'sections' ) ) {
 				the_row();
@@ -30,7 +36,7 @@ if ( have_posts() ) {
 				?>
 				<div class="section">
 					<div class="text"><?php echo $text; ?></div>
-					<div class="image" style="background-image:url(<?php echo $image; ?>);"></div>
+					<div class="image" style="background-image:url('<?php echo esc_url( $image ); ?>');"></div>
 				</div>
 				<?php
 			}
